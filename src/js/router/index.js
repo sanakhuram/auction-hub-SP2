e6f900992ddef9ea556f5111f33e00fb3f804725
Sana Khuram <sana.khuram.baig@gmail.com>
2025-01-31 18:12:02 +0100

    Added Folders, fixed router 0️⃣🐞

---
+++ src/js/router/index.js
@@ -0,0 +1,33 @@
+// This function controls which JavaScript file is loaded on which page
+// In order to add additional pages, you will need to implement them below
+// You may change the behaviour or approach of this file if you choose
+export default async function router(pathname = window.location.pathname) {
+  switch (pathname) {
+    case "/":
+      await import("./views/home.js");
+      break;
+    case "/auth/":
+      await import("./views/auth.js");
+      break;
+    case "/auth/login/":
+      await import("./views/login.js");
+      break;
+    case "/auth/register/":
+      await import("./views/register.js");
+      break;
+    case "/post/":
+      await import("./views/listing.js");
+      break;
+    case "/post/edit/":
+      await import("./views/listingEdit.js");
+      break;
+    case "/post/create/":
+      await import("./views/listingCreate.js");
+      break;
+    case "/profile/":
+      await import("./views/profile.js");
+      break;
+    default:
+      await import("./views/notFound.js");
+  }
+}
