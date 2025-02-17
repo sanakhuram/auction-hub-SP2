cea71875ef36fb329e0bec6d00f68bb053939fe1
Sana Khuram <sana.khuram.baig@gmail.com>
2025-02-17 09:55:21 +0100

    Resized images added alert to the authGurad.js 🐞

--- src/js/utilities/authGuard.js
+++ src/js/utilities/authGuard.js
@@ -1,6 +1,12 @@
+import { showAlert } from "./alert";
+
 export function authGuard() {
   if (!localStorage.token) {
-    alert("You must be logged in to view this page");
-    window.location.href = "/auth/login/";
+    
+    showAlert("You must be logged in to view this page", "warning", 5000); 
+
+    setTimeout(() => {
+      window.location.href = "/auth/login/";
+    }, 5000); 
   }
 }
